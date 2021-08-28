0073eb89c04dbcae8d6af6f1a89886c6c2353a12
michi <michi.blum@hotmail.com>
2021-08-28 21:58:11 +0200

    followed example from https://fractalideas.com/blog/making-react-and-django-play-well-together-single-page-app-model/

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,60 @@
 <template>
   <div>
-    <button @click="getPets">GET pets</button>
-    <button @click="postPet">POST pet</button>
+    <div>
+      <p>Test GET request: {{ this.testGet }}</p>
+      <p>Test POST request: {{ this.testPost }}</p>
+      <p>CSRF token: {{ this.csrfToken }}</p>
+    </div>
+<!--    <br>-->
+<!--    <button @click="getCSRFToken">GET CSRF token</button>-->
+<!--    <br>-->
+<!--    <button @click="getPets">GET pets</button>-->
+<!--    <br>-->
+<!--    <button @click="postPet">POST pet</button>-->
   </div>
 </template>
 
 <script>
 
-import axios from "axios"
+// import axios from "axios"
 export default {
   name: 'HelloWorld',
-  methods: {
-    getPets: function () {
-      axios.get(`${process.env.VUE_APP_API_URL}/pets/`, {
-        // headers: {
-        //   'X-CSRFToken': csrftoken,
-        //   'Cache-Control': 'no-cache',
-        //   'Pragma': 'no-cache',
-        // },
-        // withCredentials: true
-      }).then((res) => {
-        console.log(res)
-      }).catch((err) => {
-        console.log(err)
-      })
-    },
-    postPet: function () {
-      let data = {}
-      axios.post(`${process.env.VUE_APP_API_URL}/pets/`, data, {
-        // headers: {
-        //   'X-CSRFToken': csrftoken,
-        //   'Cache-Control': 'no-cache',
-        //   'Pragma': 'no-cache',
-        // },
-        // withCredentials: true
-      }).then((res) => {
-        console.log(res)
-      }).catch((err) => {
-        console.log(err)
-      })
+  data: function () {
+    return {
+      csrfToken: null,
+      testGet: null,
+      testPost: null,
     }
   },
+  methods: {
+    getCsrfToken: async function () {
+      if (this.csrfToken === null) {
+        const response = await fetch(`${process.env.VUE_APP_API_URL}/csrf`, {
+          credentials: 'include',
+        });
+        const data = await response.json();
+        this.csrfToken = data.csrfToken;
+      }
+      return this.csrfToken;
+    },
+    testRequest: async function (method) {
+      const response = await fetch(`${process.env.VUE_APP_API_URL}/ping`, {
+        method: method,
+        headers: (
+            method === 'POST'
+                ? {'X-CSRFToken': await this.getCsrfToken()}
+                : {}
+        ),
+        credentials: 'include',
+      });
+      const data = await response.json();
+      return data.result;
+    }
+  },
+  async mounted() {
+    this.testGet = await this.testRequest('GET')
+    this.testPost = await this.testRequest('POST')
+  }
 }
 </script>
 
