31bc3195765a91dfb451b9d73aa9b68ce7831637
michi <michi.blum@hotmail.com>
2021-08-30 11:09:43 +0200

    sessions implemented

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <div>
-      <p>Test GET request: {{ this.testGet }}</p>
-      <p>Test POST request: {{ this.testPost }}</p>
-      <p>CSRF token: {{ this.csrfToken }}</p>
+    <h1>Django Cookie Auth</h1>
+
+    <div v-if="this.isAuthenticated">
+      <p>You are logged in!</p>
+      <button @click="whoami">Who Am I?</button>
+      <button @click="logout">Log out</button>
     </div>
-<!--    <br>-->
-<!--    <button @click="getCSRFToken">GET CSRF token</button>-->
-<!--    <br>-->
-<!--    <button @click="getPets">GET pets</button>-->
-<!--    <br>-->
-<!--    <button @click="postPet">POST pet</button>-->
+    <div v-else>
+      <h2>Login</h2>
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username" v-model="username">
+      <br>
+      <label for="password">Password</label>
+      <input type="text" id="password" name="password" v-model="password">
+      <br>
+      <button @click="login">Login</button>
+    </div>
+    <p>{{ this.error }}</p>
   </div>
 </template>
 
@@ -21,39 +28,93 @@ export default {
   name: 'HelloWorld',
   data: function () {
     return {
-      csrfToken: null,
-      testGet: null,
-      testPost: null,
+      csrf: null,
+      username: "",
+      password: "",
+      error: "",
+      isAuthenticated: false
     }
   },
   methods: {
-    getCsrfToken: async function () {
-      if (this.csrfToken === null) {
-        const response = await fetch(`${process.env.VUE_APP_API_URL}/csrf`, {
-          credentials: 'include',
-        });
-        const data = await response.json();
-        this.csrfToken = data.csrfToken;
-      }
-      return this.csrfToken;
-    },
-    testRequest: async function (method) {
-      const response = await fetch(`${process.env.VUE_APP_API_URL}/ping`, {
-        method: method,
-        headers: (
-            method === 'POST'
-                ? {'X-CSRFToken': await this.getCsrfToken()}
-                : {}
-        ),
-        credentials: 'include',
+    getCSRF: function () {
+      fetch(`${process.env.VUE_APP_API_URL}/csrf/`, {
+        credentials: "include",
+      }).then((res) => {
+        let csrfToken = res.headers.get("X-CSRFToken");
+        this.csrf = csrfToken
+        console.log({csrfToken});
+      }).catch((err) => {
+        console.log({err});
       });
-      const data = await response.json();
-      return data.result;
+    },
+    getSession: function () {
+      fetch(`${process.env.VUE_APP_API_URL}/session/`, {
+        credentials: "include",
+      }).then((res) => res.json()).then((data) => {
+        console.log({data});
+        if (data.isAuthenticated) {
+          this.isAuthenticated = true
+        } else {
+          this.isAuthenticated = false
+          this.getCSRF();
+        }
+      }).catch((err) => {
+        console.log({err});
+      });
+    },
+    whoami: function () {
+      fetch(`${process.env.VUE_APP_API_URL}/whoami/`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }).then((res) => res.json()).then((data) => {
+        console.log("You are logged in as: " + data.username);
+      }).catch((err) => {
+        console.log({err});
+      });
+    },
+    isResponseOk: function (response) {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    },
+    login: function () {
+      fetch(`${process.env.VUE_APP_API_URL}/login/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": this.csrf,
+        },
+        credentials: "include",
+        body: JSON.stringify({username: this.username, password: this.password}),
+      }).then(this.isResponseOk).then((data) => {
+        console.log({data});
+        this.isAuthenticated = true
+        this.username = ""
+        this.password = ""
+        this.error = ""
+      }).catch((err) => {
+        console.log({err});
+        this.error = "Wrong username or password."
+      });
+    },
+    logout: function () {
+      fetch(`${process.env.VUE_APP_API_URL}/logout/`, {
+        credentials: "include",
+      }).then(this.isResponseOk).then((data) => {
+        console.log({data});
+        this.isAuthenticated = false
+        this.getCSRF();
+      }).catch((err) => {
+        console.log({err});
+      });
     }
   },
-  async mounted() {
-    this.testGet = await this.testRequest('GET')
-    this.testPost = await this.testRequest('POST')
+  mounted() {
+    this.getSession()
   }
 }
 </script>
